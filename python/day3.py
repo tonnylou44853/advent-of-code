6a81f982eca80f7317a9a41706d5396f21cc68f4
Tonny Lou <69238318+tonnylou44853@users.noreply.github.com>
2022-12-07 12:48:05 +0000

    Improve day 3 solution.

--- python/day3.py
+++ python/day3.py
@@ -2,23 +2,10 @@
 
 from functools import reduce
 
-def read_data(data_path: str):
-    with open(data_path) as f:
-        extracted_data = [line.strip('\n') for line in f.readlines()]
-    return extracted_data
-
-def find_common_item_part1(data: list):
-    splitted_data = [[d[:int(len(d)/2)], d[int(len(d)/2):]] for d in data]
+def find_common_item(data: list):
     common_item = []
-    for data in splitted_data:
-        common_item.append(list(reduce(lambda i, j: i & j, (set(x) for x in data))))
-    return [item for items in common_item for item in items]
-
-def find_common_item_part2(data: list):
-    splitted_data = list(zip(*[iter(data)]*3))
-    common_item = []
-    for data in splitted_data:
-        common_item.append(list(reduce(lambda i, j: i & j, (set(x) for x in data))))
+    for d in data:
+        common_item.append(list(reduce(lambda i, j: i & j, (set(x) for x in d))))
     return [item for items in common_item for item in items]
 
 def assign_priorities(data: list):
@@ -32,12 +19,13 @@ def assign_priorities(data: list):
 
 def main():
     data_path = './data/day3_input.txt'
-    data = read_data(data_path=data_path)
-    common_item_part1 = find_common_item_part1(data=data) 
-    common_item_part2 = find_common_item_part2(data=data)  
-    priorities1 = assign_priorities(common_item_part1)
-    priorities2 = assign_priorities(common_item_part2)
+    with open(data_path) as f:
+        data = [line.strip('\n') for line in f.readlines()]
+    part1_data = [[d[:int(len(d)/2)], d[int(len(d)/2):]] for d in data]
+    priorities1 = assign_priorities(find_common_item(data=part1_data))
     print("Part 1: ", sum(priorities1))
+    part2_data = list(zip(*[iter(data)]*3))
+    priorities2 = assign_priorities(find_common_item(data=part2_data))
     print("Part 2: ", sum(priorities2))
 
 if __name__ == '__main__':
