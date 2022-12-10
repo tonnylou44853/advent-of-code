2d6e2caa3ff086bca054c22628051fba24bbe663
Tonny Lou <69238318+tonnylou44853@users.noreply.github.com>
2022-12-10 11:24:33 +0000

    Remove redundant code in day 1 solution again.

--- 2022/python/day1.py
+++ 2022/python/day1.py
@@ -4,13 +4,10 @@ def main():
     data_path = './data/day1_input.txt'
     with open(data_path) as f:
         contents = [line.splitlines() for line in f.read().split('\n\n')]
-        carlories_list = [sum(list(map(int, line))) for line in contents]
-    print("Part 1:", max(carlories_list))
-    top_n_carlories = 0
-    for _ in range(3):
-        top_n_carlories += max(carlories_list)
-        carlories_list.remove(max(carlories_list))
-    print("Part 2:", top_n_carlories)
+    carlories_list = [sum(list(map(int, line))) for line in contents]
+    carlories_list.sort(reverse=True)
+    print("Part 1:", carlories_list[0])
+    print("Part 2:", sum(carlories_list[:3]))
 
 if __name__=='__main__':
     main()
